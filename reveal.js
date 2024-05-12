9fe7d50d91406f76d6c8698f1490ae1418276e67
Ruan <rugcosta1234@gmail.com>
2024-05-12 10:56:41 -0300

    Add: adicionando efeito nos containers

---
+++ reveal.js
@@ -0,0 +1,114 @@
+// Chamando todo o documento da pagina
+window.revelar = ScrollReveal({ reset: true });
+
+// TOPO DO SITE
+
+revelar.reveal(".efect-txt-main", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal(".efect-img-main", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+});
+
+// TITULOS
+
+revelar.reveal(".skills-subTitle", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal(".about-title", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal(".project-title", {
+  duration: 2000,
+  distance: "90px",
+});
+revelar.reveal(".efect-txt-contact", {
+  duration: 2000,
+  distance: "90px",
+});
+
+// CARDS PROJECT
+
+revelar.reveal(".efect-card1", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+});
+
+revelar.reveal(".efect-card2", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1000,
+});
+
+revelar.reveal(".efect-card3", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1500,
+});
+
+//SESÁO SKILLS
+revelar.reveal(".ctn-social", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+});
+
+//  SESSÃO SOBRE
+
+revelar.reveal(".efect-img-about", {
+  duration: 1000,
+  distance: "90px",
+  delay: 500,
+  origin: "left",
+});
+
+revelar.reveal(".efect-txt-about", {
+  duration: 1000,
+  distance: "90px",
+  delay: 1000,
+  origin: "right",
+});
+
+// SESSÃO FOOTER
+
+revelar.reveal(".contact-container", {
+  duration: 1000,
+  distance: "90px",
+  delay: 1000,
+  origin: "bottom",
+});
+revelar.reveal(".efect-input-name", {
+  duration: 1000,
+  distance: "90px",
+  delay: 500,
+  origin: "bottom",
+});
+
+revelar.reveal(".efect-input-email", {
+  duration: 1000,
+  distance: "90px",
+  delay: 1000,
+  origin: "bottom",
+});
+revelar.reveal(".efect-textarea-message", {
+  duration: 1000,
+  distance: "90px",
+  delay: 1500,
+  origin: "bottom",
+});
+
+revelar.reveal(".efect-footer-button", {
+  duration: 1000,
+  distance: "90px",
+  delay: 2000,
+  origin: "bottom",
+});
